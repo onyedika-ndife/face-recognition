fb76f5af90964469aa258b093f1703d9291abee3
ndife-onyedika <onyedikandife@gmail.com>
2019-09-11 15:49:13 +0100

    Added Id to parameters

--- register/trainer.py
+++ register/trainer.py
@@ -4,29 +4,28 @@ import numpy as np
 from PIL import Image
 
 
-def train(name):
+def train(Id, name):
     recognizer = cv2.face.LBPHFaceRecognizer_create()
 
     path = f"./assets/face_data/student/{name}"
 
     if not os.path.exists("./assets/training_data"):
         os.makedirs("./assets/training_data")
+    else:
+        pass
 
-    def getImagesWithID(path):
-        imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    def get_images_with_id(path):
+        image_paths = [os.path.join(path, f) for f in os.listdir(path)]
         faces = []
-        IDs = []
-        for imagePath in imagePaths:
-            faceImg = Image.open(imagePath).convert("L")
-            faceNp = np.array(faceImg, "uint8")
-            ID = int(os.path.split(imagePath)[-1].split(".")[1])
-            faces.append(faceNp)
-            IDs.append(ID)
-            cv2.imshow("Training", faceNp)
-            cv2.waitKey(10)
-        return np.array(IDs), faces
+        _ids = []
+        for image_path in image_paths:
+            face_img = Image.open(image_path).convert("L")
+            face_array = np.array(face_img, "uint8")
+            _id = Id
+            faces.append(face_array)
+            _ids.append(_id)
+        return np.array(_ids), faces
 
-    Ids, faces = getImagesWithID(path)
+    Ids, faces = get_images_with_id(path)
     recognizer.train(faces, Ids)
     recognizer.save(f"./assets/training_data/{name}-training_data.yml")
-    cv2.destroyAllWindows()
