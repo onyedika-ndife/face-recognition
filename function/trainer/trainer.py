14139325a5e627ab790308a09f806c4b6d9096af
ndife-onyedika <onyedikandife@gmail.com>
2019-09-12 04:04:34 +0100

    Converted Trainer Func to a Class

--- function/trainer/trainer.py
+++ function/trainer/trainer.py
@@ -4,28 +4,34 @@ import numpy as np
 from PIL import Image
 
 
-def train(Id, name):
-    recognizer = cv2.face.LBPHFaceRecognizer_create()
+class TRAINER:
+    def __init__(self, Id, name):
+        super().__init__()
 
-    path = f"./assets/face_data/student/{name}"
+        self.train(Id, name)
 
-    if not os.path.exists("./assets/training_data"):
-        os.makedirs("./assets/training_data")
-    else:
-        pass
+    def train(self, Id, name):
+        self.recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-    def get_images_with_id(path):
-        image_paths = [os.path.join(path, f) for f in os.listdir(path)]
-        faces = []
-        _ids = []
-        for image_path in image_paths:
-            face_img = Image.open(image_path).convert("L")
-            face_array = np.array(face_img, "uint8")
-            _id = Id
-            faces.append(face_array)
-            _ids.append(_id)
-        return np.array(_ids), faces
+        self._dir = f"./assets/face_data/student/{name}"
 
-    Ids, faces = get_images_with_id(path)
-    recognizer.train(Ids, faces)
-    recognizer.save(f"./assets/training_data/{name}-training_data.yml")
+        if not os.path.exists("./assets/training_data"):
+            os.makedirs("./assets/training_data")
+        else:
+            pass
+
+        def get_images_with_id(_dir):
+            image_paths = [os.path.join(_dir, f) for f in os.listdir(_dir)]
+            faces = []
+            _ids = []
+            for image_path in image_paths:
+                face_img = Image.open(image_path).convert("L")
+                face_array = np.array(face_img, "uint8")
+                _id = Id
+                faces.append(face_array)
+                _ids.append(_id)
+            return np.array(_ids), faces
+
+        Ids, faces = get_images_with_id(self._dir)
+        self.recognizer.train(Ids, faces)
+        self.recognizer.save(f"./assets/training_data/{name}-training_data.yml")
